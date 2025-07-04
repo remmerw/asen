6399971b174da824780a367d67731344b54f0f3e
remmerw <remmer.wilts@gmail.com>
2025-07-04 18:17:33 +0200

    Version 0.3.4 ------------- - Update dependencies - Cleanup hostname

--- asen/src/jvmMain/kotlin/io/github/remmerw/asen/Asen.jvm.kt
+++ asen/src/jvmMain/kotlin/io/github/remmerw/asen/Asen.jvm.kt
@@ -3,6 +3,11 @@ import io.ktor.network.sockets.InetSocketAddress
 import java.net.InetAddress
 
 actual fun createInetSocketAddress(address: ByteArray, port:Int) : InetSocketAddress {
-    val inet = InetAddress.getByAddress(address)
-    return InetSocketAddress(inet.hostName, port)
+    if(address.size == 16) {
+        val inet = InetAddress.getByAddress(address)
+        return InetSocketAddress(inet.hostName, port)
+    } else {
+        val host = io.github.remmerw.asen.core.hostname(address)
+        return InetSocketAddress(host, port)
+    }
 }
